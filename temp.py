44bc881a6c606732f0fd65b4abcf91226f156b6e
Adham Jalaal <49797431+profadham@users.noreply.github.com>
2025-02-23 09:07:46 +0200

    Add files via upload

--- temp.py
+++ temp.py
@@ -4,21 +4,28 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
 import matplotlib.pyplot as plt
 import numpy as np
 
-#n = np.arange(-5,5)
-#x = np.reshape([n==0],np.shape(n))
-#x = n-2==0
-#plt.stem(n, x,use_line_collection='True',markerfmt = 'rx',label='lab2',linefmt='r--')
 
-n = np.arange(-25,25)
-x = np.exp(12j*n)*(n>=0)
-y = np.real(x)
-z = np.imag(x)
-plt.subplot(2,1,1) #2,1 2 rows and 1 column
-plt.stem(n, y,use_line_collection='True')
-plt.subplot(2,1,2)
-plt.stem(n,z,use_line_collection='True')
-plt.legend() #makes the label appear
+def mul(n1,x1,n2,x2):
+    min_n = min(min(n1),min(n2))
+    max_n = max(max(n1),max(n2))
+    n =np.arange(min_n,max_n+1)
+    ###################################
+    x1n = np.zeros(np.shape(n))
+    x1n[(n>=min(n1))&(n<=max(n1))]=x1
+    x2n = np.zeros(np.shape(n))
+    x2n[(n>=min(n2))&(n<=max(n2))]=x2
+    ###################################
+    x = x1n*x2n
+    return x,n
+
+n1 = np.arange(0,5)
+x1 = n1
+n2 = np.arange(-2,3)
+x2 = np.full(np.shape(n2),2)
+
+x,n = mul(n1,x1,n2,x2)
+plt.stem(n, x,use_line_collection='True')
+    
