13c3dc450d4b0b6ac797c9d15f25a85776de629f
Adham Jalaal <49797431+profadham@users.noreply.github.com>
2025-04-27 08:49:39 +0300

    Update Fourier_Series.py

--- Fourier_Series.py
+++ Fourier_Series.py
@@ -1,12 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
 
 from sympy import fourier_series, pi, plot
 from sympy.abc import x
-f = x*x
+f = x**2
 s = fourier_series(f,(x,-pi,pi))
 s1 = s.truncate(n=3)
 s2 = s.truncate(n=5)
 s3 = s.truncate(n=10)
-p = plot(f,s1,s2,s3,(x,-pi,pi),ylim=[-10,10],show=False,legend=True,title="lab task")
+p = plot(f,s1,s2,s3,(x,-pi,pi),ylim=None,show=False,legend=True,title="lab task")
 
 p[0].line_color = (0, 0, 0)
 p[0].label = 'f'
